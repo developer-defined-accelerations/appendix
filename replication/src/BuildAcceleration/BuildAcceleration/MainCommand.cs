d034f5dba697442417ef90048d04861295da1cbb
Mingyang Yin <mingyang.yin@uwaterloo.ca>
2023-10-07 04:20:00 +0000

    update code for long-tail

--- replication/src/BuildAcceleration/BuildAcceleration/MainCommand.cs
+++ replication/src/BuildAcceleration/BuildAcceleration/MainCommand.cs
@@ -1,6 +1,7 @@
 using System.Threading.Tasks;
 using ForecastBuildTime.AccelerationProcessing;
 using ForecastBuildTime.AccelerationSampling;
+using ForecastBuildTime.DataProcessing;
 using McMaster.Extensions.CommandLineUtils;
 
 namespace ForecastBuildTime;
@@ -8,6 +9,7 @@ namespace ForecastBuildTime;
 [Subcommand(typeof(DetectRules))]
 [Subcommand(typeof(KMeansCluster))]
 [Subcommand(typeof(CountCategories))]
+[Subcommand(typeof(LongTail))]
 internal class MainCommand
 {
     private Task OnExecuteAsync()
